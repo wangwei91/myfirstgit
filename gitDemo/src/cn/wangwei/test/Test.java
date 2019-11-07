c00a4aff72affb16e752007917f1dca92b4512a2
ww <ww@wangwei>
2019-11-07 21:42:56 +0800

    新增测试类

--- gitDemo/src/cn/wangwei/test/Test.java
+++ gitDemo/src/cn/wangwei/test/Test.java
@@ -1,7 +1,19 @@
 package cn.wangwei.test;
 
+import java.io.PrintStream;
+
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("git");
+		int a = 10;
+		System.out.println(a);
+	}
+	
+	public void method(int a) {
+		System.setOut(new PrintStream(System.out) {
+				@Override
+				public void println(String str) {
+					
+				}
+		});
 	}
 }
